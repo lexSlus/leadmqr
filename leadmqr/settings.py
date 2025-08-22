13bcba5328fb59028bc44b33c9bb0ab147c9a2c1
Sloboda <sloboda276@gmail.com>
2025-08-22 16:09:05 +0300

    app

--- leadmqr/settings.py
+++ leadmqr/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'leads',
-    'django_celery_beat'
+    'django_celery_beat',
+    'ai_calls',
+    'rest_framework',
 
 ]
 
